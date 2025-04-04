88d5b73c680db66c7d03af4244fbafb5818a3354
Your Name <z5503831@unsw.edu.au>
2025-04-04 14:25:37 +1100

    endpoint error check

--- src/routes.py
+++ src/routes.py
@@ -13,7 +13,13 @@ routes = Blueprint("routes", __name__)
 
 
 DYNAMODB_ENDPOINT = os.environ.get("DYNAMODB_ENDPOINT")
-dynamodb = boto3.resource("dynamodb", region_name="ap-southeast-2", endpoint_url=DYNAMODB_ENDPOINT) if DYNAMODB_ENDPOINT else boto3.resource("dynamodb", region_name="ap-southeast-2")
+
+if DYNAMODB_ENDPOINT:
+    dynamodb = boto3.resource("dynamodb", region_name="ap-southeast-2", endpoint_url=DYNAMODB_ENDPOINT)
+else:
+    dynamodb = boto3.resource("dynamodb", region_name="ap-southeast-2")
+    print(DYNAMODB_ENDPOINT)
+
 TABLE_NAME = "StockAnalytics"
 table = dynamodb.Table(TABLE_NAME)
 
